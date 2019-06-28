8199e84f2a93d441b2916d1f407e29d8872c88c4
Indra Lukmana <indralukmana@live.com>
2019-06-29 00:09:45 +0700

    🎉  Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <header className="app-header">React Imager</header>
+    </div>
+  );
+};
+
+export default App;
