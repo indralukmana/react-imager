7dc98f8e683ecb386763b367cddf5c2c49f1ee25
Indra Lukmana <indralukmana@live.com>
2019-06-29 11:47:23 +0700

    ♻ IMPROVE: Add typing for React Component and type attribute to button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import './App.css';
-import sampleImage from './sample.jpg'
+import sampleImage from './sample.jpg';
 
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <header className="app-header">React Imager</header>
+interface Props {
+  [n: string]: never;
+}
 
-      <img src={sampleImage} alt='sample' style={{width: '50%'}}></img>
+const App: React.FunctionComponent<
+  Props
+> = (): JSX.Element => (
+  <div className="app">
+    <header className="app-header">React Imager</header>
 
-      <div className="btn-groups-manipulate">
-        <button>Rotate Left</button>
-        <button>Rotate Right</button>
-        <button>Scale Up</button>
-        <button>Scale Down</button>
-      </div>
+    <img
+      src={sampleImage}
+      alt="sample"
+      style={{ width: '50%' }}
+    />
 
-        <button>Finish</button>
+    <div className="btn-groups-manipulate">
+      <button type="button">Rotate Left</button>
+      <button type="button">Rotate Right</button>
+      <button type="button">Scale Up</button>
+      <button type="button">Scale Down</button>
     </div>
-  );
-};
+
+    <button type="button">Finish</button>
+  </div>
+);
 
 export default App;
