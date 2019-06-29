3669b41f476ffc3d579108d9ab7cf408b6210fe1
Indra Lukmana <indralukmana@live.com>
2019-06-29 20:06:39 +0700

    ✨ NEW: Add rotation feature

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,14 @@ import sampleImage from './sample.jpg';
 const App = (): JSX.Element => {
   const [rotation, setRotation] = useState<number>(0);
 
+  const rotate = (degree: number) => {
+    if (rotation > -360 && rotation < 360) {
+      setRotation(rotation + degree);
+    } else {
+      setRotation(degree);
+    }
+  };
+
   return (
     <div className="app">
       <header className="app-header">React Imager</header>
@@ -13,15 +21,20 @@ const App = (): JSX.Element => {
         <AvatarEditor image={sampleImage} rotate={rotation} />
       </div>
 
-    <div className="btn-groups-manipulate">
-      <button type="button">Rotate Left</button>
-      <button type="button">Rotate Right</button>
-      <button type="button">Scale Up</button>
-      <button type="button">Scale Down</button>
-    </div>
+      <div className="btn-groups-manipulate">
+        <button type="button" onClick={() => rotate(-10)}>
+          Rotate Left
+        </button>
+        <button type="button" onClick={() => rotate(10)}>
+          Rotate Right
+        </button>
+        <button type="button">Scale Up</button>
+        <button type="button">Scale Down</button>
+      </div>
 
-    <button type="button">Finish</button>
-  </div>
-);
+      <button type="button">Finish</button>
+    </div>
+  );
+};
 
 export default App;
