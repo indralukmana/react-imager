5ec4c48d8c28b3b212b9d5e9154670fa0e9dcfed
Indra Lukmana <indralukmana@live.com>
2019-06-29 20:04:37 +0700

    ✨ NEW: Add the image container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import AvatarEditor from 'react-avatar-editor';
 import sampleImage from './sample.jpg';
 
 const App = (): JSX.Element => {
+  const [rotation, setRotation] = useState<number>(0);
+
   return (
     <div className="app">
       <header className="app-header">React Imager</header>
       <div className="image-container">
+        <AvatarEditor image={sampleImage} rotate={rotation} />
+      </div>
 
     <div className="btn-groups-manipulate">
       <button type="button">Rotate Left</button>
