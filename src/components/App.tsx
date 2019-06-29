490fd04cebbfb8e341d4fb73cadcd1876fd11eb9
Indra Lukmana <indralukmana@live.com>
2019-06-29 10:48:03 +0700

    💄 NEW: Add basic layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import sampleImage from './sample.jpg'
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <header className="app-header">React Imager</header>
+
+      <img src={sampleImage} alt='sample' style={{width: '50%'}}></img>
+
+      <div className="btn-groups-manipulate">
+        <button>Rotate Left</button>
+        <button>Rotate Right</button>
+        <button>Scale Up</button>
+        <button>Scale Down</button>
+      </div>
+
+        <button>Finish</button>
     </div>
   );
 };
