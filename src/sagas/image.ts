87926fe1a427758a83c92474003cdb377cf9a531
Indra Lukmana <indralukmana@live.com>
2019-07-05 22:41:17 +0700

    ⚗ NEW: Switch fetch with apisauce

--- src/sagas/image.ts
+++ src/sagas/image.ts
@@ -1,19 +1,32 @@
 import { call, takeEvery, all } from 'redux-saga/effects';
+import { create } from 'apisauce';
 
-const url = 'http://localhost:5555/image/uploadbase';
+const url = 'http://localhost:5555';
+const api = create({
+  baseURL: url,
+  headers: { Accept: '*/*' },
+});
 
-const uploadImage = payload => {
-  console.log(JSON.stringify(payload));
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(res => {
-    console.log(res);
-    return res.json();
+const uploadImage = async payload => {
+  const data = JSON.stringify(payload);
+
+  // fetch(url, {
+  //   method: 'POST',
+  //   body: JSON.stringify(payload),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // }).then(res => {
+  //   console.log(res);
+  //   return res.json();
+  // });
+
+  const result = await api.post('/image/uploadbase', data, {
+    headers: { 'Content-Type': 'application/json' },
   });
+
+  // console.log(result);
+  return result;
 };
 
 function* handleUploadImage(action) {
