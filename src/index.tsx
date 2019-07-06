9b2227d499ba6e11d82812eab2323e40340ef67f
Indra Lukmana <indralukmana@live.com>
2019-07-06 17:24:04 +0700

    ➕ NEW: Add reactstrap dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
